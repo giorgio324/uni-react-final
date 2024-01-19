843cf9170ecc789ce5327cc5429279ac914bfc7b
giorgio324 <giorgikochuashvili2002@gmail.com>
2024-01-20 00:57:15 +0400

    added data and filtering checkboxes functionality

--- src/components/FiltersDisplay.jsx
+++ src/components/FiltersDisplay.jsx
@@ -1,8 +1,21 @@
 import { useFilterContext } from "../context/FilterContext";
 
 const FiltersDisplay = () => {
-  const { searchValue, setSearchValue, categoriesLoading, categories } =
-    useFilterContext();
+  const {
+    searchValue,
+    setSearchValue,
+    categoriesLoading,
+    categories,
+    setSelectedCategories,
+  } = useFilterContext();
+  const handleCategoryChange = (event) => {
+    const category = event.target.value;
+    if (event.target.checked) {
+      setSelectedCategories((prev) => [...prev, category]);
+    } else {
+      setSelectedCategories((prev) => prev.filter((c) => c !== category));
+    }
+  };
   if (categoriesLoading) {
     return (
       <div className="w-[230px] border p-4 flex flex-col gap-4">
@@ -12,7 +25,7 @@ const FiltersDisplay = () => {
   }
   return (
     <div className="w-[230px] border p-4 flex flex-col gap-4">
-      <label className="flex flex-col gap-2 text-xl font-semibold">
+      <label className="flex flex-col gap-2 text-lg font-semibold">
         Search
         <input
           type="text"
@@ -24,8 +37,13 @@ const FiltersDisplay = () => {
       <div className="flex flex-col">
         {categories?.categoryList.map((categorie, index) => {
           return (
-            <label key={index}>
-              <input type="checkbox" name="" value={categorie} />
+            <label key={index} className="flex gap-4 text-base font-semibold">
+              <input
+                type="checkbox"
+                name=""
+                value={categorie}
+                onChange={handleCategoryChange}
+              />
               {categorie}
             </label>
           );
--- src/context/FilterContext.jsx
+++ src/context/FilterContext.jsx
@@ -7,17 +7,35 @@ export const FilterProvider = ({ children }) => {
   const { data: products, isLoading: productsLoading } = useFetch("/products");
   const { data: categories, isLoading: categoriesLoading } =
     useFetch("/categories");
+
   const [searchValue, setSearchValue] = useState("");
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
   const filteredProducts = products?.filter((product) => {
-    if (!searchValue) return true;
-    if (product.title.toLowerCase().includes(searchValue.toLowerCase())) {
-      return true;
+    // If search box is filled, check if product title includes search value
+    if (searchValue) {
+      if (!product.title.toLowerCase().includes(searchValue.toLowerCase())) {
+        return false;
+      }
     }
+
+    // If checkboxes are selected, check if product category contains the selected categories
+    if (selectedCategories.length > 0) {
+      if (!selectedCategories.includes(product.category)) {
+        return false;
+      }
+    }
+
+    // If none of the above conditions fail, the product passes the filters
+    return true;
   });
+
   return (
     <FilterContext.Provider
       value={{
         products,
+        setSelectedCategories,
+        selectedCategories,
         categories,
         categoriesLoading,
         searchValue,
